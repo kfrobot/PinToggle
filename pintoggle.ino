ce3eb73c1c93ead562876879d24dc53a08d8969d
Lasse Lukkari <lasse.lukkari@gmail.com>
2012-12-19 13:45:58 +0200

    Fixed settings loading

--- pintoggle.ino
+++ pintoggle.ino
@@ -4,15 +4,14 @@
 #include "aJSON.h"
 #include "WebServer.h"
 
-
 //define range of pins you want to control
 #define FROM_PIN 55
 #define TO_PIN 58
 
 byte mac[] = { 0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x02 };
-aJsonObject* pins;
-WebServer webserver("", 80);
 
+WebServer webserver("", 80);
+aJsonObject* pins;
 File file;
 
 void indexCmd(WebServer &server, WebServer::ConnectionType type, char *, bool) {
@@ -20,26 +19,26 @@ void indexCmd(WebServer &server, WebServer::ConnectionType type, char *, bool) {
 	if (type != WebServer::HEAD) {
 		P(helloMsg) =
 				"<!DOCTYPE html>\n<html lang=\"en\">\n"
-				"<head>\n"
-				"\t<meta charset=\"utf-8\">\n"
-				"\t<title>Pin Toggle</title>\n"
-				"\t<link href=\"http://www.cs.helsinki.fi/u/ljlukkar/pincontrol/css/style.css\" rel=\"stylesheet\">\n"
-				"\t<link href=\'http://fonts.googleapis.com/css?family=Open+Sans+Condensed:700,300\' rel=\'stylesheet\' type=\'text/css\'/>\n"
-				"</head>\n"
-				"<body>\n"
-				"<div class=\"container\">\n"
-				"\t<div id=\"pin-list\">\n"
-				"\t</div>\n"
-				"</div>\n"
-				"<script src=\"http://www.cs.helsinki.fi/u/ljlukkar/pincontrol/js/jquery.min.js\"></script>\n"
-				"<script src=\"http://www.cs.helsinki.fi/u/ljlukkar/pincontrol/js/lodash.min.js\"></script>\n"
-				"<script src=\"http://www.cs.helsinki.fi/u/ljlukkar/pincontrol/js/lib/backbone-min.js\"></script>\n"
-				"<script src=\"http://www.cs.helsinki.fi/u/ljlukkar/pincontrol/js/models/pin.js\"></script>\n"
-				"<script src=\"http://www.cs.helsinki.fi/u/ljlukkar/pincontrol/js/collections/pins.js\"></script>\n"
-				"<script src=\"http://www.cs.helsinki.fi/u/ljlukkar/pincontrol/js/views/pins.js\"></script>\n"
-				"<script src=\"http://www.cs.helsinki.fi/u/ljlukkar/pincontrol/js/views/app.js\"></script>\n"
-				"<script src=\"http://www.cs.helsinki.fi/u/ljlukkar/pincontrol/js/app.js\"></script>\n</body>\n"
-				"</html>";
+						"<head>\n"
+						"\t<meta charset=\"utf-8\">\n"
+						"\t<title>Pin Toggle</title>\n"
+						"\t<link href=\"http://www.cs.helsinki.fi/u/ljlukkar/pincontrol/css/style.css\" rel=\"stylesheet\">\n"
+						"\t<link href=\'http://fonts.googleapis.com/css?family=Open+Sans+Condensed:700,300\' rel=\'stylesheet\' type=\'text/css\'/>\n"
+						"</head>\n"
+						"<body>\n"
+						"<div class=\"container\">\n"
+						"\t<div id=\"pin-list\">\n"
+						"\t</div>\n"
+						"</div>\n"
+						"<script src=\"http://www.cs.helsinki.fi/u/ljlukkar/pincontrol/js/jquery.min.js\"></script>\n"
+						"<script src=\"http://www.cs.helsinki.fi/u/ljlukkar/pincontrol/js/lodash.min.js\"></script>\n"
+						"<script src=\"http://www.cs.helsinki.fi/u/ljlukkar/pincontrol/js/lib/backbone-min.js\"></script>\n"
+						"<script src=\"http://www.cs.helsinki.fi/u/ljlukkar/pincontrol/js/models/pin.js\"></script>\n"
+						"<script src=\"http://www.cs.helsinki.fi/u/ljlukkar/pincontrol/js/collections/pins.js\"></script>\n"
+						"<script src=\"http://www.cs.helsinki.fi/u/ljlukkar/pincontrol/js/views/pins.js\"></script>\n"
+						"<script src=\"http://www.cs.helsinki.fi/u/ljlukkar/pincontrol/js/views/app.js\"></script>\n"
+						"<script src=\"http://www.cs.helsinki.fi/u/ljlukkar/pincontrol/js/app.js\"></script>\n</body>\n"
+						"</html>";
 		server.printP(helloMsg);
 	}
 }
@@ -48,12 +47,11 @@ void indexCmd(WebServer &server, WebServer::ConnectionType type, char *, bool) {
 // server.setPreventDefault(true); disables the default prosessing and responce once
 void switchBeforeResposeCmd(WebServer &server, WebServer::ConnectionType type, char * tail, bool tailComplete,
 		aJsonObject ** collection, aJsonObject ** model) {
- 	if (type == WebServer::PUT) {
+	if (type == WebServer::PUT) {
 		if (model != NULL) {
 			pinMode(aJson.getObjectItem(*model, "pin")->valueint, (bool) aJson.getObjectItem(*model, "low")->valuebool);
 		}
 	}
-
 
 }
 
@@ -82,6 +80,15 @@ void setup() {
 		file = SD.open("pin.txt");
 		pins = aJson.parse(file);
 		file.close();
+
+		aJsonObject* current = pins->child;
+
+		while (current) {
+			pinMode(aJson.getObjectItem(current, "pin")->valueint,
+					(bool) aJson.getObjectItem(current, "low")->valuebool);
+			current = current->next;
+		}
+
 	} else { //if not found populate dta
 		pins = aJson.createArray();
 
